3cd9d9481c28f59c14b924af68a53a54098f22b8
danielrispler <danielrispler@gmail.com>
2024-12-07 17:58:37 +0200

    fix reviews

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { CommonQuestionsComponent } from './common-questions/common-questions.component';
 import { CoursesComponent } from './courses-tab/courses-tab.component';
 import { ReviewsComponent } from './reviews/reviews.component';
@@ -8,7 +7,7 @@ import { WhyUsComponent } from './why-us/why-us.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MatIconModule, CoursesComponent, WhyUsComponent, CommonQuestionsComponent, ReviewsComponent],
+  imports: [CoursesComponent, WhyUsComponent, CommonQuestionsComponent, ReviewsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
