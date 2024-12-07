0d6a94127d569d8b9e6dc929ea289e67b126bb2a
danielrispler <danielrispler@gmail.com>
2024-12-07 23:35:55 +0200

    finish landing page

---
+++ src/app/features/landing-page/general-info/general-info.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-info',
+  imports: [NgOptimizedImage],
+  templateUrl: './general-info.component.html',
+  styleUrl: './general-info.component.scss'
+})
+export class GeneralInfoComponent {
+
+}
--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { CommonQuestionsComponent } from './common-questions/common-questions.co
 import { CoursesComponent } from './courses-tab/courses-tab.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { WhyUsComponent } from './why-us/why-us.component';
+import { GeneralInfoComponent } from './general-info/general-info.component';
 
 
 @Component({
   selector: 'app-landing-page',
-  imports: [CoursesComponent, WhyUsComponent, CommonQuestionsComponent, ReviewsComponent],
+  imports: [CoursesComponent, WhyUsComponent, CommonQuestionsComponent, ReviewsComponent, GeneralInfoComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
